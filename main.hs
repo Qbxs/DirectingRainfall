617ef8ba83de40893721f47fa434eb748525c348
Pascal Engel <ip4ssi@gmail.com>
2019-11-15 13:15:08 +0100

    specify import

--- main.hs
+++ main.hs
@@ -1,5 +1,5 @@
 import Data.List
-import InputParser
+import InputParser (Input(..), Range, Point, Tarp(..), lexer, inputParser)
 
 -- Algorithm:
 -- 1. Find upmost tarp which has no tarp above it at any point
@@ -39,8 +39,8 @@ upper t1@(T (a1,a2) (b1,b2)) t2@(T (c1,c2) (d1,d2))
     Nothing    -> a2 >= c2
     Just (x,y) -> a2 >= d2 ||
                   if c2 >= b2 then False else
-             -- here: all cases for which t1 overlaps t2 partially
-             -- consider all combinations of {a,b} x {c,d} => 8
+               -- here: all cases for which t1 overlaps t2 partially
+               -- consider all combinations of {a,b} x {c,d} => 8
                      x == a1 && y == c1 && a2 >= c2
                   || x == a1 && y == d1 && b2 >= d2
                   || x == b1 && y == c1 && b2 >= c2
@@ -49,7 +49,7 @@ upper t1@(T (a1,a2) (b1,b2)) t2@(T (c1,c2) (d1,d2))
                   || x == c1 && y == b1 && b2 >= c2
                   || x == d1 && y == a1 && a2 >= c2
                   || x == d1 && y == b1 && b2 >= d2
-             -- here: all cases of total overlap where t1 is higher
+               -- here: all cases of total overlap where t1 is higher
                   || x == a1 && y == b1 && a2 >= c2
                   || x == b1 && y == a1 && a2 >= c2
                   || x == c1 && y == d1 && b2 >= d2
@@ -57,17 +57,16 @@ upper t1@(T (a1,a2) (b1,b2)) t2@(T (c1,c2) (d1,d2))
 
 
 quicksort, maxSort :: (a -> a -> Bool) -> [a] -> [a]
-
 -- this does not do the trick :(
 quicksort p []     = []
 quicksort p (x:xs) = lesser ++ [x] ++ greater
-             where lesser = quicksort p [a | a <- xs, not $ p x a]
-                   greater = quicksort p [a | a <- xs, p x a]
+               where lesser = quicksort p [a | a <- xs, not $ p x a]
+                     greater = quicksort p [a | a <- xs, p x a]
 
 -- less eficient but safe sort (isUpper is not transitive)
 maxSort p [] = []
 maxSort p l  = (fst $ maxList l):maxSort p (snd $ maxList l)
-         where maxList [x]      = (x,[])
+         where maxList [x]    = (x,[])
                maxList (x:xs) = if p x (fst $ maxList xs)
                                 then (x,(fst $ maxList xs):(snd $ maxList xs))
                                 else (fst $ maxList xs,x:(snd $ maxList xs))
@@ -101,7 +100,7 @@ weigh (a,b) ((S (x1,x2) o):xs) = []
 
 
 
--- TODO: implement a topsort to find out the order of reachable tarps
+-- TODO: implement a topsort to find out the order of reachable tarps (x)
 -- Steps: - Look for highest Tarp (= tarp with highest upper point) which is
 --          also in our [a,b] interval
 --        -> First Element of topsorted tarps (ignore everything before)
