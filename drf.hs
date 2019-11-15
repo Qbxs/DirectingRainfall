08d7d3c1ae2156bc844943e1cc9a7f2a0685f805
Pascal Engel <ip4ssi@gmail.com>
2019-11-15 22:17:13 +0100

    type signatures

--- drf.hs
+++ drf.hs
@@ -6,6 +6,7 @@ import System.Exit
 main :: IO()
 main = getArgs >>= parse >>= (print . (map simplify) . sortInput . inputParser . lexer)
 
+parse :: [String] -> IO String
 parse ["-h"]        = usage   >> exit
 parse ["--help"]    = usage   >> exit
 parse ["-v"]        = version >> exit
@@ -13,7 +14,10 @@ parse ["--version"] = version >> exit
 parse []            = getContents
 parse fs            = concat `fmap` mapM readFile fs
 
+usage, version :: IO()
 usage   = putStrLn "Usage: drf [-vh] [file ..]"
 version = putStrLn "Haskell drf 0.1 2019"
+
+exit, die :: IO String
 exit    = exitWith ExitSuccess
 die     = exitWith (ExitFailure 1)
