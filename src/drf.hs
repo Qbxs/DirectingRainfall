9b74406ffd02e28fd184e5089659e1ac6c04055a
Pascal Engel <ip4ssi@gmail.com>
2019-11-19 19:14:36 +0100

    simplify functionality added

--- src/drf.hs
+++ src/drf.hs
@@ -1,4 +1,4 @@
-import Tarps
+import Tarps hiding (simplify)
 import InputParser (Input(..), Range, Point, Tarp(..), lexer, inputParser)
 import Exports
 import Data.Aeson
@@ -9,7 +9,7 @@ import System.Process
 import System.Directory
 
 main :: IO()
-main = getArgs >>= parse >>= (print . (map simplify) . sortInput . inputParser . lexer)
+main = getArgs >>= parse >>= (print . sortInput . inputParser . lexer)
 
 parse :: [String] -> IO String
 parse ["-h"]             = usage   >> exit
@@ -18,6 +18,8 @@ parse ["-v"]             = version >> exit
 parse ["--version"]      = version >> exit
 parse ("-i":fs)          = (parse fs) >>= input
 parse ("--input":fs)     = (parse fs) >>= input
+parse ("-s":fs)          = (parse fs) >>= simplify
+parse ("--simplify":fs)  = (parse fs) >>= simplify
 parse []                 = getContents
 parse fs                 = concat `fmap` mapM readFile fs
 
@@ -25,7 +27,7 @@ usage, version :: IO()
 usage   = putStrLn "Usage: drf [-vhi] [file ..]"
 version = putStrLn "Haskell drf 0.1 2019"
 
-input :: String -> IO String
+input,simplify :: String -> IO String
 input fs = do
   inp <- return fs
   (writeInput . inJ . inputParser . lexer) inp
@@ -33,9 +35,13 @@ input fs = do
   putStrLn "Succesfully opened inputCanvas.html"
   exit
 
+simplify fs = do
+  inp <- return fs
+  (writeSimple . siJ . inputParser . lexer) inp
+  system "open js/simpleCanvas.html"
+  putStrLn "Succesfully opened simpleCanvas.html"
+  exit
+
 exit, die :: IO String
 exit    = exitWith ExitSuccess
 die     = exitWith (ExitFailure 1)
-
-scnd :: [String]-> IO [String]
-scnd xs = return $ tail xs
--- src/exports.hs
+++ src/exports.hs
@@ -14,7 +14,7 @@ data JInput = JInput {
       b     :: Int,
       tarps :: [[Int]]
       } deriving (Show,Generic)
-instance ToJSON JInput where
+instance ToJSON JInput
 
 inJ :: Input -> JInput
 inJ (Input (a,b) _ ts) = JInput a b (map (\(T (x1,y1) (x2,y2)) -> [x1,y1,x2,y2]) ts)
@@ -24,4 +24,22 @@ writeInput s = do -- very inefficient
   encodeFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/temp.json" s
   i <- readFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/temp.json"
   writeFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/input.json" ("input="++i)
+  removeFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/temp.json"
+
+
+data JSimple = JSimple {
+    sa       :: Int,
+    sb       :: Int,
+    starps   :: [[Int]]
+} deriving (Show,Generic)
+instance ToJSON JSimple
+
+siJ :: Input -> JSimple
+siJ (Input (a,b) _ ts) = JSimple a b (map (\(S (x,y) o) -> [x,y,if o == L then 0 else 1]) (map simplify (reverse (maxSort upper ts))))
+
+writeSimple :: JSimple -> IO ()
+writeSimple s = do -- very inefficient
+  encodeFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/temp.json" s
+  i <- readFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/temp.json"
+  writeFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/simple.json" ("simple="++i)
   removeFile "/Users/pascalengel/Documents/acmContest/DirectingRainfall/js/temp.json"
