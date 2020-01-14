8d478249b250b7972a5d52c89c66a55c48a4135f
Pascal Engel <ip4ssi@gmail.com>
2020-01-14 18:03:50 +0100

    optimize pointInside

--- src/Geometry.hs
+++ src/Geometry.hs
@@ -20,8 +20,16 @@ area (Tri p1 p2 p3) = sqrt $ s * (s-a) * (s-b) * (s-c)
                             c = length p3 p1
                             s = (a+b+c)/2
 
+-- area of a right-angled triangle
+-- assume right angle is at p2
+areaRight :: Triangle -> Double
+areaRight (Tri p1 p2 p3) = a*b/2
+                     where a = length p1 p2
+                           b = length p2 p3
+
 -- check whether a point is inside a triangle
+-- assume triangle is right-angled (at p2)
 pointInside :: Point -> Triangle -> Bool
-pointInside q (Tri p1 p2 p3) = area (Tri p1 p2 p3) == sum [area $ Tri q p1 p2,
-                                                           area $ Tri q p1 p3,
-                                                           area $ Tri q p2 p3]
+pointInside q (Tri p1 p2 p3) = areaRight (Tri p1 p2 p3) == sum [area $ Tri q p1 p2,
+                                                                area $ Tri q p1 p3,
+                                                                area $ Tri q p2 p3]
